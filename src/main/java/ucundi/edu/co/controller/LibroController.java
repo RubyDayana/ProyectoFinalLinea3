22e651df0cc20659accb09711c0454ea3f67d7e3
RubyDayana <cardenas.ruby.10@gmail.com>
2021-11-24 11:55:29 -0500

    cambio de autorización Libo

--- src/main/java/ucundi/edu/co/controller/LibroController.java
+++ src/main/java/ucundi/edu/co/controller/LibroController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,7 @@ import ucundi.edu.co.entity.Libro;
 import ucundi.edu.co.exception.ModelNotFoundException;
 import ucundi.edu.co.service.ILibroService;
 
+@PreAuthorize("hasAuthority('Autor')")
 @RequestMapping("/libro")
 @RestController
 @Validated
